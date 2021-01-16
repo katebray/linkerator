ff913d5a20439de403121250ae3e811225ac14d7
Kaitlin Bray <Kaitlin.bray@yahoo.com>
2021-01-16 10:57:33 -0500

    routes error

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,19 @@
 import axios from 'axios';
 
+//    const { data } = await axios.get('/routes/links');
 export async function getLinks() {
   try {
-    const { data } = await axios.get('/routes/index');
+    const { data } = await axios.get('/api/links');
     return data;
   } catch (error) {
     console.error('getLinks api error', error);
   }
 }
 
+//    const { data } = await axios.post('/routes/links', {
 export async function createNewLink(title, date, descript, tags) {
   try {
-    const { data } = await axios.post('/routes/index', {
+    const { data } = await axios.post('/api/links', {
       title,
       date,
       descript,
@@ -23,9 +25,10 @@ export async function createNewLink(title, date, descript, tags) {
   }
 }
 
+// const { data } = await axios.get('/routes/tags');
 export async function getTags() {
   try {
-    const { data } = await axios.get('/routes/index');
+    const { data } = await axios.get('/api/tags');
     return data;
   } catch (error) {
     console.error('getTags api error', error);
